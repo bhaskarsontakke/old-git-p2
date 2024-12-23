1bde7ac736b4ec18d47e7852d81f8ceb65a90685
seeded-enterprise-owner <bhaskar.sontakke@smartbear.com>
2024-12-24 00:14:00 +0530

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjEyOTQ/spectral.js?branch_slug=main&token=def8a33e-f9e0-4a9b-ab0b-ac2a0c27b882";
+export default { extends: ruleset };
